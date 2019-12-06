b40ac156e1dc346d331321c18e5a8acc4e9d8750
Denis Zayakin <denis1998zayakin@gmail.com>
2019-12-06 17:59:58 +0200

    added scrollView

---
+++ OnlineStore/MainViewController/MainViewController.swift
@@ -0,0 +1,65 @@
+//
+//  FirstViewController.swift
+//  OnlineStore
+//
+//  Created by Denis Zayakin on 12/6/19.
+//  Copyright © 2019 Denis Zayakin. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var slides:[Slide] = []
+    
+    override func viewDidLoad() {
+        scrollView.delegate = self
+        scrollView.showsHorizontalScrollIndicator = false
+        
+        super.viewDidLoad()
+        super.viewDidLoad()
+        slides = createSlides()
+        setupSlideScrollView(slides: slides)
+        
+        pageControl.numberOfPages = slides.count
+        pageControl.currentPage = 0
+        view.bringSubviewToFront(pageControl)
+    }
+
+    func createSlides() -> [Slide] {
+        
+        let slide1:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide1.imageView.image = UIImage(named: "1")
+        
+        
+        let slide2:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide2.imageView.image = UIImage(named: "2")
+        
+        let slide3:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide3.imageView.image = UIImage(named: "3")
+        
+        return [slide1, slide2, slide3]
+    }
+    
+    func setupSlideScrollView(slides : [Slide]) {
+        scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
+        scrollView.isPagingEnabled = true
+        
+        for i in 0 ..< slides.count {
+            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
+            scrollView.addSubview(slides[i])
+        }
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x/view.frame.width)
+        pageControl.currentPage = Int(pageIndex)
+    }
+
+}
+
---
+++ OnlineStore/MainViewController/Slide.swift
@@ -0,0 +1,15 @@
+//
+//  Slide.swift
+//  OnlineStore
+//
+//  Created by Denis Zayakin on 12/6/19.
+//  Copyright © 2019 Denis Zayakin. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
